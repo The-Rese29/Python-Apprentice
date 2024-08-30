434e828b0684ef9ad118de6ed9227977fbbdf9f9
The-Rese29 <theresen@saintjamesacademy.com>
2024-08-30 19:11:45 +0000

    Hi, The-Rese29, this was your first day at Python Coding (8/30/24). If you ever see this when you're all grown up, try to see how far you've come. Tina.exitonclick() Always goes at the end! :)

--- lessons/00_Turtles/04a_Turtle_Tricks.py
+++ lessons/00_Turtles/04a_Turtle_Tricks.py
@@ -12,12 +12,35 @@ to see how to use the turtle commands.
 import turtle                           # Tell Python we want to work with the turtle
 turtle.setup (width=600, height=600)    # Set the size of the window
 
-tina = turtle.Turtle()                  # Create a turtle named tina
+tina = turtle.Turtle()  
+                # Create a turtle named tina
 
 # Use tina.forward() and tina.left() to draw a triangle
 # Make each side of the triangle a different color with 
 # tina.pencolor()
 
-... # Your code here
+tina.shape('turtle')
+tina.speed(2)
 
-turtle.exitonclick()                    # Close the window when we click on it
+tina.pencolor('blue')
+tina.pendown()
+tina.forward(90)
+
+tina.left(120)
+tina.forward(90)
+tina.left(120)
+tina.forward(90)
+tina.penup()
+tina.right(120)
+tina.forward(90)
+tina.left(60)
+tina.pendown()
+tina.forward(50)
+tina.right(90)
+tina.forward(200)
+tina.left(90)
+tina.forward(50)
+tina.left(90)
+tina.forward(200)
+
+turtle.exitonclick()

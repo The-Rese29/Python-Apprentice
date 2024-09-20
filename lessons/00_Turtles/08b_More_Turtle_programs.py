e55baf5230a815ea476b15b1e2c521dd9cecdfc4
The-Rese29 <theresen@saintjamesacademy.com>
2024-09-20 04:54:11 +0000

    At home 9/19/24-Image changed

--- lessons/00_Turtles/08b_More_Turtle_programs.py
+++ lessons/00_Turtles/08b_More_Turtle_programs.py
@@ -1,7 +1,7 @@
 """
 
-Copy the code from the previous lesson, 08a_More_Turtle_programs.ipynb, 
-from the section "Change the Turtle Image"
+#Copy the code from the previous lesson, 08a_More_Turtle_programs.ipynb, 
+#from the section "Change the Turtle Image"
 
 
 Then change the code so that the turtle has a different image ( look in the 'images'
@@ -9,10 +9,11 @@ directory ) and moves to the corners of the screen in a square pattern.
 """
 
 import turtle
-def set_turtle_image()
+def set_turtle_image(turtle, image_name):
+    """Set the turtle's shape to a custom image."""
 
     from pathlib import Path
-    image_dir = Path(__file__).parent / image
+    image_dir = Path(__file__).parent / "images"
     image_path = str(image_dir / image_name)
 
     screen = turtle.getscreen()
@@ -24,7 +25,7 @@ screen.setup(width=600, height=600)
 
 t = turtle.Turtle()
 
-set_turtle_image(t, pikachu.gif)
+set_turtle_image(t, "pikachu.gif")
 
 t.penup()
 t.speed(3)

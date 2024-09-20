784e273729f6f56681f7b0a296f7e8392517ee9e
The-Rese29 <theresen@saintjamesacademy.com>
2024-09-20 19:13:43 +0000

    Week 3 or 4- moustache :)

--- lessons/00_Turtles/09_LeagueBot.py
+++ lessons/00_Turtles/09_LeagueBot.py
@@ -17,4 +17,36 @@ screen.bgcolor('white')
 
 t = turtle.Turtle()
 
+
 ... # Your Code Here
+
+import turtle
+def set_turtle_image(turtle, image_name):
+    """Set the turtle's shape to a custom image."""
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+
+t = turtle.Turtle()
+
+t.shape("turtle")
+#set_turtle_image(t, "leaguebot_bolt.gif")
+
+t.turtlesize(stretch_wid=5, stretch_len=5, outline=5)
+
+t.pencolor('blue')
+
+for i in range(6):
+    t.forward(40)
+    t.right(60)
+
+
+turtle.exitonclick()

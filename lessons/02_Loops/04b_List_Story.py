b42efbedf9c0d83804f9ae8c4a664fb9aca96965
The-Rese29 <theresen@saintjamesacademy.com>
2024-10-18 19:13:18 +0000

    Week 8? Loop stuff.

--- lessons/02_Loops/04b_List_Story.py
+++ lessons/02_Loops/04b_List_Story.py
@@ -9,7 +9,7 @@ append them to the story
 words = ['Once', '👦', 'upon', '🐕', 'park', 'met', 'with', 'a', 'the', 
     'time', 'to', 'who', '🐈', '👧', 'and', 'went', 'had', 'play', '⚽.', 'they']
 
-story = words[0]+ " " + words[2] + " " + words[7] + " " + words[9] + " " + words[1] + " " + words[15] + " " + words[10] + " " + words[8] + " " + words[4] + " " + words[14] + " " + words[5] + "dog"
+story = words[0]+ " " + words[2] + " " + words[7] + " " + words[9] + " " + words[1] + " " + words[15] + " " + words[10] + " " + words[8] + " " + words[4] + " " + words[14] + " " + words[5] + " "
 
 #print(words[0])
 #print(words[2])

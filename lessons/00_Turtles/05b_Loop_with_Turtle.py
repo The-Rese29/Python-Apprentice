407d3931f8380d15126dda67daeeb00eea8f837f
The-Rese29 <theresen@saintjamesacademy.com>
2024-11-01 19:12:35 +0000

    Accidentally skipped types and logic, now I have to go back and do it :(

--- lessons/00_Turtles/05b_Loop_with_Turtle.py
+++ lessons/00_Turtles/05b_Loop_with_Turtle.py
@@ -28,5 +28,8 @@ tina.left(90)
 tina.forward(150)
 tina.left(90)
 
+for i in range(4):
+    tina.forward(150)
+    tina.left(90)
 
 turtle.exitonclick()                    # Close the window when we click on it
--- lessons/02_Loops/04b_List_Story.py
+++ lessons/02_Loops/04b_List_Story.py
@@ -9,7 +9,7 @@ append them to the story
 words = ['Once', '👦', 'upon', '🐕', 'park', 'met', 'with', 'a', 'the', 
     'time', 'to', 'who', '🐈', '👧', 'and', 'went', 'had', 'play', '⚽.', 'they']
 
-story = words[0]+ " " + words[2] + " " + words[7] + " " + words[9] + " " + words[1] + " " + words[15] + " " + words[10] + " " + words[8] + " " + words[4] + " " + words[14] + " " + words[5] + " "
+story = words[0]+ " " + words[2] + " " + words[7] + " " + words[9] + " " + words[1] + " " + words[15] + " " + words[10] + " " + words[8] + " " + words[4] + " " + words[14] + " " + words[5] + " " + words[7] + " " + words[3] + " " + "which bit his leg and made him cry."
 
 #print(words[0])
 #print(words[2])
